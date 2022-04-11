d0843052065ecc6ffd5be8a22f868ef1fdb176e2
Rinku921234 <rinku921234@gmail.com>
2022-04-11 13:28:49 +0530

    homepage and cardview

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Try from './pages/try';
+import Carousels from './pages/carousels';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+    
+  </Routes>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import { MoralisProvider } from "react-moralis";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MoralisProvider appId="xxxxxx" serverUrl="xxxxx">
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </MoralisProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );

2d981ed344eb123a80a41b779dd48ab8edbaab62
anilkumar9622 <anilchauhan.src@gmail.com>
2022-04-12 18:49:01 +0530

    proper work

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import Drawer1 from './common/sidemenu/drawer1'
 import './App.css';
-
-// import Echo from './common/echo'
 import { useRoutes } from 'react-router-dom';
-// import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
 
 function App() {
   const routes = useRoutes([
     {path:"/",element:<Drawer1/>},
-    // {path:"/echo",element:<Echo/>}
+
   ])
   return routes
 }
--- src/common/sidemenu/drawer1.jsx
+++ src/common/sidemenu/drawer1.jsx
@@ -13,19 +13,27 @@ export default function Drawer1() {
 
   const [closable, setClosable] = useState(false)
   const [openDrawer, setopenDrawer] = useState(false)
-  const [openSubDrawer, setopenSubDrawer] = useState(false)
+  const [openSubDrawer, setopenSubDrawer] = useState(null)
 
   const showDrawer = () => {
     setopenDrawer(!openDrawer);
   };
+  console.log(openDrawer);
 
-  const handleOnClose = () => {
-    setClosable(!closable)
-  };
+  // const handleOnClose = () => {
+  //   setClosable(!closable)
+  // };
 
   const openSidemenu = () => {
        setopenSubDrawer(!openSubDrawer)
-  }
+      }
+      console.log(openSubDrawer);
+ 
+
+  
+  
+    
+    
 
 
   return (
@@ -37,17 +45,19 @@ export default function Drawer1() {
 
       <Drawer style={{ padding: '0px' }}
         placement="left"
+        // width={'50%'}
         closable={false}
         maskClosable={true}
         onClose={() => { setopenDrawer(false) }}
         visible={openDrawer}
+        // style={{position:"relative"}}
       >
 
         
           <div class='header'style={{ background: '#232f3e', width: '365px', height: '50px', position: 'fixed' }} >  
             <h2 style={{ padding: '8px 0px 0px 35px', margin: '0px', color: 'white', alignItem: 'center', gap: '5px', fontWeight: '700' }} >{<UserOutlined />}  Hello, Sign in</h2> 
             </div>
-            <div class='sidemenu'  >
+            <div class='sidemenu' style={{position:'relative'}} >
           <div className='borderBtm' style={{ top: '0px', fontFamily: ' "Amazon Ember",Arial,sans-serif' }}>
           <div style={{
             fontSize: '18px', padding: '30px 0px 5px 36px', borderBottom: '2px', 
@@ -79,10 +89,10 @@ export default function Drawer1() {
             display: 'inline-block'
           }} >    Digital Content And Devices     </div>
           <div  style={{ display: "flex", flexDirection: "column", lineHeight: '40px', fontSize: '14px', color: "black", borderBottom: '1px' }}>
-            <a href={openSidemenu} onclick="submenu();" onClick={openSidemenu} style={{ display: "flex", padding: '0px 36px ', justifyContent: "space-between" }}>
+            <a href={openSidemenu} onClick={openSidemenu} style={{ display: "flex", padding: '0px 36px ', justifyContent: "space-between" }}>
               <a    className='items'>Echo & Alexa </a><Icon className='icon' component={RightOutlined} />
             </a>
-            <a href='' style={{ display: "flex", padding: '0px 36px', justifyContent: "space-between" }}>
+            <a href={openSidemenu}  onClick={openSidemenu} style={{ display: "flex", padding: '0px 36px', justifyContent: "space-between" }}>
               <a className='items'>Fire TV </a><Icon className='icon' component={RightOutlined} />
             </a>
             <a href='' style={{ display: "flex", padding: '0px 36px', justifyContent: "space-between" }}>
@@ -179,7 +189,7 @@ export default function Drawer1() {
           </div>
         </div>
         
-      <div class='subsidemenu' id="subsidemenuId" onClick >   
+        {/* <div className='subsidemenu' style={{position:'absolute'}}> */}
       <Drawer style={{ padding: '0px' }}
         placement="right"
         closable={false}
@@ -187,67 +197,68 @@ export default function Drawer1() {
         onClose={() => { setopenSubDrawer(false) }}
         visible={openSubDrawer}
         getContainer={false}
+      
         
       >
 
         <div className='borderBtm' style={{ top: '0px', fontFamily: ' "Amazon Ember",Arial,sans-serif' }}>
-          {/* <div style={{ background: '#232f3e', width: '365px', height: '50px', position: 'fixed' }} >    <h2 style={{ padding: '8px 0px 0px 35px', margin: '0px', color: 'white', alignItem: 'center', gap: '5px', fontWeight: '700' }} >{<UserOutlined />}  Hello, Sign in</h2>     </div> */}
-          <a href={!openSidemenu} visible={false} onClick={!openSidemenu} onclick= 'backbtn();' style={{ display: "flex", padding: '0px 42px ', justifyContent: "space-between", fontSize:'25px', gap:'7px', paddingBottom:'5px' }}>
-          <Icon className='icon' component={ArrowLeftOutlined} /> <a className='items'>Main Menu </a>
-            </a><hr/>
-         
-          <div style={{
-            fontSize: '18px', padding: '20px 0px 5px 42px', borderBottom: '2px',
+          <a   onClick={openSidemenu} className='items' 
+           style={{ display: "flex", padding: '0px 42px ', justifyContent: "", fontSize:'23px', gap:'7px', paddingBottom:'5px' }}>
+          <Icon className='icon' component={ArrowLeftOutlined} /> Main Menu 
+            </a>
+            <hr/>
+            <div style={{
+            fontSize: '18px', padding: '30px 0px 5px 36px', borderBottom: '2px', 
             fontWeight: '700',
             lineHeight: '24px',
             color: '#111',
             textTransform: 'capitalize',
             display: 'inline-block'
           }} >    Trending     </div>
-          <div   style={{ display: "flex", flexDirection: "column", lineHeight: '40px', fontSize: '14px', paddingBottom: '5px', color: "black", borderBottom: '#ddd' }}>
+          <div style={{ display: "flex", flexDirection: "column", lineHeight: '40px', fontSize: '14px', paddingBottom: '5px', color: "black", borderBottom: '#ddd' }}>
 
-            <a href=''  style={{ display: "flex", padding: '0px 42px ', justifyContent: "space-between" }}>
-              <a onClick={showDrawer} className='items'>Best Sellers </a>
+            <a href='' style={{ display: "flex", padding: '0px 36px ', justifyContent: "space-between" }}>
+              <a className='items'>Best Sellers </a>
             </a>
-            <a href='' style={{ display: "flex", padding: '0px 42px ', justifyContent: "space-between" }}>
+            <a href='' style={{ display: "flex", padding: '0px 36px ', justifyContent: "space-between" }}>
               <a className='items'>New Releases </a>
             </a>
-            <a href='' style={{ display: "flex", padding: '0px 42px ', justifyContent: "space-between" }}>
+            <a href='' style={{ display: "flex", padding: '0px 36px ', justifyContent: "space-between" }}>
               <a className='items'>Movers and Shakers</a>
             </a>
           </div>
           <hr />
 
           <div className='borderBtm' style={{
-            fontSize: '18px', padding: '20px 0px 5px 42px', borderBottom: '2px', fontWeight: '700',
+            fontSize: '18px', padding: '10px 0px 5px 36px', borderBottom: '2px', fontWeight: '700',
             lineHeight: '24px',
             color: '#111',
             textTransform: 'capitalize',
             display: 'inline-block'
           }} >    Digital Content And Devices     </div>
           <div  style={{ display: "flex", flexDirection: "column", lineHeight: '40px', fontSize: '14px', color: "black", borderBottom: '1px' }}>
-            <a href='' style={{ display: "flex", padding: '0px 42px ', justifyContent: "space-between" }}>
-              <a className='items'>Echo & Alexa </a><Icon className='icon' component={RightOutlined} />
+            <a href={openSidemenu} onClick={openSidemenu} style={{ display: "flex", padding: '0px 36px ', justifyContent: "space-between" }}>
+              <a    className='items'>Echo & Alexa </a><Icon className='icon' component={RightOutlined} />
             </a>
-            <a href='' style={{ display: "flex", padding: '0px 42px', justifyContent: "space-between" }}>
+            <a href='' style={{ display: "flex", padding: '0px 36px', justifyContent: "space-between" }}>
               <a className='items'>Fire TV </a><Icon className='icon' component={RightOutlined} />
             </a>
-            <a href='' style={{ display: "flex", padding: '0px 42px', justifyContent: "space-between" }}>
+            <a href='' style={{ display: "flex", padding: '0px 36px', justifyContent: "space-between" }}>
               <a className='items'>Kindle E-Readers & eBooks </a><Icon className='icon' component={RightOutlined} />
             </a>
-            <a href='' style={{ display: "flex", padding: '0px 42px', justifyContent: "space-between" }}>
+            <a href='' style={{ display: "flex", padding: '0px 36px', justifyContent: "space-between" }}>
               <a className='items'>Audible Audiobooks</a><Icon className='icon' component={RightOutlined} />
             </a>
-            <a href='' style={{ display: "flex", padding: '0px 42px', justifyContent: "space-between" }}>
+            <a href='' style={{ display: "flex", padding: '0px 36px', justifyContent: "space-between" }}>
               <a className='items'>Amazon Prime Video </a><Icon className='icon' component={RightOutlined} />
             </a>
-            <a href='' style={{ display: "flex", padding: '0px 42px', justifyContent: "space-between" }}>
+            <a href='' style={{ display: "flex", padding: '0px 36px', justifyContent: "space-between" }}>
               <a className='items'>Amazon Prime Music </a><Icon className='icon' component={RightOutlined} />
             </a>
           </div><hr />
 
           <div className='borderBtm' style={{
-            fontSize: '18px', padding: '20px 0px 5px 42px', borderBottom: '2px', fontWeight: '700',
+            fontSize: '18px', padding: '10px 0px 5px 36px', borderBottom: '2px', fontWeight: '700',
             lineHeight: '24px',
             color: '#111',
             textTransform: 'capitalize',
@@ -255,17 +266,16 @@ export default function Drawer1() {
           }} >Shop By Department</div>
           <div style={{ display: "flex", flexDirection: "column", lineHeight: '40px', fontSize: '14px', paddingBottom: '5px', color: "black", borderBottom: '2px' }}>
 
-            <a href='' style={{ display: "flex", padding: '0px 42px', justifyContent: "space-between" }}>
+            <a href='' style={{ display: "flex", padding: '0px 36px', justifyContent: "space-between" }}>
               <a className='items'>Mobiles, Computers </a><Icon className='icon' component={RightOutlined} />
             </a>
-
-            <a href='' style={{ display: "flex", padding: '0px 42px', justifyContent: "space-between" }}>
+            <a href='' style={{ display: "flex", padding: '0px 36px', justifyContent: "space-between" }}>
               <a className='items'>TV, Appliances, Electronics  </a><Icon className='icon' component={RightOutlined} />
             </a>
-            <a href='' style={{ display: "flex", padding: '0px 42px', justifyContent: "space-between" }}>
+            <a href='' style={{ display: "flex", padding: '0px 36px', justifyContent: "space-between" }}>
               <a className='items'>Men's Fashion </a><Icon className='icon' component={RightOutlined} />
             </a>
-            <a href='' style={{ display: "flex", padding: '0px 42px', justifyContent: "space-between" }}>
+            <a href='' style={{ display: "flex", padding: '0px 36px', justifyContent: "space-between" }}>
               <a className='items'>Women's Fashion </a><Icon className='icon' component={RightOutlined} />
             </a>
             {/* <a href=''    className='items'>See All <br /></a> */}
@@ -283,53 +293,15 @@ export default function Drawer1() {
 
           </div><hr />
 
-          <div className='borderBtm' style={{
-            fontSize: '18px', padding: '20px 0px 5px 42px', borderBottom: '2px', fontWeight: '700',
-            lineHeight: '24px',
-            color: '#111',
-            textTransform: 'capitalize',
-            display: 'inline-block'
-          }} >    Programs & Features    </div>
-          <div style={{ display: "flex", flexDirection: "column", lineHeight: '40px', fontSize: '14px', paddingBottom: '10px', color: "black", display: 'grid', direction: 'column', borderBottom: '2px' }}>
-
-            <a href='' style={{ display: "flex", padding: '0px 42px', justifyContent: "space-between" }}>
-              <a className='items'>Gift Cards & Mobile Recharges </a><Icon className='icon' component={RightOutlined} />
-            </a>
-            <a href='' style={{ display: "flex", padding: '0px 42px', justifyContent: "space-between" }}>
-              <a className='items'>Flight Tickets </a>
-            </a>
-            <a href='' style={{ display: "flex", padding: '0px 42px', justifyContent: "space-between" }}>
-              <a className='items'>#FoundItOnAmazon</a>
-            </a>
-            <a href='' style={{ display: "flex", padding: '0px 42px', justifyContent: "space-between" }}>
-              <a className='items'>Clearance store</a>
-            </a>
-          </div><hr />
-
-          <div style={{
-            fontSize: '18px', borderBottom: '2px', padding: '20px 0px 5px 42px', fontWeight: '700',
-            lineHeight: '24px',
-            color: '#111',
-            textTransform: 'capitalize',
-            display: 'inline-block'
-          }} >    Help & Settings     </div>
-          <div style={{ display: "flex", flexDirection: "column", lineHeight: '40px', fontSize: '14px', paddingBottom: '18px', color: "#111111", display: 'grid', direction: 'column', borderBottom: '2px' }}>
-
-            <a href='' style={{ display: "flex", padding: '0px 42px', justifyContent: "space-between" }}>
-              <a className='items'>Your Account</a>
-            </a>
-            <a href='' style={{ display: "flex", padding: '0px 42px', justifyContent: "space-between" }}>
-              <a className='items'>Customer Service</a>
-            </a>
-            <a href='' style={{ display: "flex", padding: '0px 42px', justifyContent: "space-between" }}>
-              <a className='items'>Sign In </a>
-            </a>
-          </div>
-        </div>
+            </div>
+          
+        
         
       </Drawer>
       </div>
-      </div>
+      
+      {/* </div> */}
+     
       </Drawer>
     </>
   );
